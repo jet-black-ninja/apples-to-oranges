a39534b9fcb120477618cc90c1043727ee3b024e
jet_black_ninja <pramodkmr958@gmail.com>
2024-09-04 12:11:27 +0530

    components done

---
+++ src/components/Store/Store.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { slide as BurgerMenu } from "react-burger-menu";
+import styles from "./Store.module.scss";
+import "./burgerMenu.scss";
+import Sidebar from "../Sidebar/SideBar";
+import FruitSection from "../FruitSection/FruitSection";
+import BurgerIcon from "../../icons/BurgerIcon";
+import CloseIcon from "../../icons/CloseIcon";
+
+
+const Store = () => {
+    const [showBurger, setShowBurger]= useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setShowBurger(window.innerWidth<940);       
+        }
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return() => window.removeEventListener("resize", handleResize);
+    },[])
+    
+    return(
+        <div className={styles.store}>
+            {showBurger ?(
+                <BurgerMenu 
+                customBurgerIcon={<BurgerIcon/>}
+                customCrossIcon = {<CloseIcon/>}
+                width={"auto"}>
+                <Sidebar />
+                </BurgerMenu>
+            ):(
+                <>
+                <Sidebar />
+                </>
+            )}
+            <FruitSection />
+        </div>
+    );
+};
+export default Store;
